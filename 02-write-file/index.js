921cf4f6f4e3c88444fbe4698708e578787e177e
antasth <antasth@mail.ru>
2023-05-04 17:15:21 +0300

    feat: complete 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const readline = require('readline')
+const path = require('path')
+const outputFile = fs.createWriteStream(
+  path.join(__dirname, 'text.txt'),
+  'utf-8'
+)
+const { stdin: input, stdout: output } = require('node:process')
+
+const rl = readline.createInterface({ input, output })
+
+const enterText = () => {
+  process.on('exit', () => {
+    console.log('Goodbye')
+    process.exit()
+  })
+  rl.question('', (inputText) => {
+    if (inputText === 'exit') {
+      rl.close()
+    } else {
+      outputFile.write(inputText + '\n')
+      enterText()
+    }
+  })
+}
+console.log('Hello')
+enterText()
