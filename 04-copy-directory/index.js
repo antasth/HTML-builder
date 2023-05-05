0de0938847bc93b47246972251e3c4e22cc99c37
antasth <antasth@mail.ru>
2023-05-05 14:30:28 +0300

    feat: complete 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,36 @@
+let fs = require('fs')
+const fsPromises = fs.promises
+const path = require('path')
+const dirPath = path.join(__dirname, 'files')
+const dirCopyPath = path.join(__dirname, 'files-copy')
 
+const copyFiles = (pathFrom, pathTo) => {
+  fs.readdir(pathFrom, (error, files) => {
+    if (error) throw error
+
+    files.forEach((file) => {
+      let filePath = path.join(pathFrom, file)
+      fs.stat(filePath, (error, stats) => {
+        if (error) throw error
+        if (stats.isFile()) {
+          fs.copyFile(
+            path.join(pathFrom, file),
+            path.join(pathTo, file),
+            (error) => {
+              if (error) throw error
+            }
+          )
+        }
+      })
+    })
+  })
+}
+
+fsPromises
+  .mkdir(dirCopyPath, { recursive: true })
+  .then(function () {
+    copyFiles(dirPath, dirCopyPath)
+  })
+  .catch(function () {
+    console.log('failed to create directory')
+  })
